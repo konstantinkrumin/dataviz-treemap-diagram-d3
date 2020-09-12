689dc8a81bdc451816ad1941a354ea63194dcd85
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-12 21:46:12 +0300

    Updates

---
+++ precache-manifest.e3f8e3f5ac7ef5213905b376ef7d28c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2472b6c86ac3cb5752b121499f0ceaca",
+    "url": "/dataviz-treemap-diagram-d3/index.html"
+  },
+  {
+    "revision": "0a8a2d7bc79fbb725e58",
+    "url": "/dataviz-treemap-diagram-d3/static/css/main.376ef88d.chunk.css"
+  },
+  {
+    "revision": "7c04b3b227c0b27ed27d",
+    "url": "/dataviz-treemap-diagram-d3/static/js/2.6a895240.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dataviz-treemap-diagram-d3/static/js/2.6a895240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a8a2d7bc79fbb725e58",
+    "url": "/dataviz-treemap-diagram-d3/static/js/main.c54142c6.chunk.js"
+  },
+  {
+    "revision": "68acc853a1448b336db8",
+    "url": "/dataviz-treemap-diagram-d3/static/js/runtime-main.d2755c03.js"
+  }
+]);
